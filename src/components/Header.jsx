b59d2d4272ce75da4c2412247ff2ca4001c93acd
Ivanovo2022 <golovushkinb@mail.ru>
2022-11-23 11:07:46 +0300

    First commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+        <img src='https://graciamarcom.com/wp-content/uploads/2018/02/Logo-design.png' />
+    </header>
+}
+
+export default Header;
