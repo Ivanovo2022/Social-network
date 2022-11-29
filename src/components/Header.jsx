7cebccebad3c5a1965a8255e845c514c11421a61
Ivanovo2022 <golovushkinb@mail.ru>
2022-11-29 09:54:17 +0300

    14_Modules and classes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import classes from "./Header.module.css";
 
 const Header = () => {
-    return <header className='header'>
+    return <header className={classes.header}>
         <img src='https://graciamarcom.com/wp-content/uploads/2018/02/Logo-design.png' />
     </header>
 }
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import classes from "./Profile.module.css"
 
 const Profile = () => {
-    return <div className='content'>
-    <div>
-      <img src='../img/company name logo.jpg' />
+    return <div className={classes.content}>
+    <div >
+        <img src="https://vsegda-pomnim.com/uploads/posts/2022-04/1649278047_1-vsegda-pomnim-com-p-krasivii-plyazh-s-palmami-foto-1.jpg" />
     </div>
     <div>
       ava + description
